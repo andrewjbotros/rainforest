7adb68734d272cf17bef8a7260e45f9d4b7ce40e
andrewjbotros <andrewjbotros@gmail.com>
2014-02-06 17:38:57 -0500

    Generating the products controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create!(
+    :name  => "Stereo",
+    :description => "Electronics",
+    :price_in_cents => "100",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+  )
+
+Product.create!(
+    :name  => "Apples",
+    :description => "Food",
+    :price_in_cents => "200",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+  )
+
+Product.create!(
+    :name  => "Golf Clubs",
+    :description => "Sporting Goods",
+    :price_in_cents => "300",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+  )
